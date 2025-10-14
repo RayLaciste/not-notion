c6f6ba8a8cf4edd0e7b1fa150ece57ca0bc774eb
Ray Laciste <67614706+RayLaciste@users.noreply.github.com>
2025-10-14 14:25:10 -0700

    Add Authentication with GitHub & Google on Landing Page

--- app/(landingpage)/_components/navbar.tsx
+++ app/(landingpage)/_components/navbar.tsx
@@ -1,12 +1,20 @@
 "use client";
 
+import { useConvexAuth } from "convex/react";
+import { SignInButton, UserButton } from "@clerk/clerk-react";
+import Link from "next/link";
+
 import { useScrollTop } from "@/hooks/use-scroll-top";
 import { ModeToggle } from "@/components/mode-toggle";
 import { cn } from "@/lib/utils";
 
 import { Logo } from "./logo";
+import { Button } from "@/components/ui/button";
+
+import { Spinner } from "@/components/spinner";
 
 export const Navbar = () => {
+  const { isAuthenticated, isLoading } = useConvexAuth();
   const scrolled = useScrollTop();
   return (
     <div
@@ -17,6 +25,31 @@ export const Navbar = () => {
     >
       <Logo />
       <div className="md:ml-auto md:justify-end justify-between w-full flex items-center gap-x-2">
+        {isLoading && (
+          <p>
+            <Spinner />
+          </p>
+        )}
+        {!isAuthenticated && !isLoading && (
+          <>
+            <SignInButton mode="modal">
+              <Button variant="ghost" size="sm">
+                Log in
+              </Button>
+            </SignInButton>
+            <SignInButton mode="modal">
+              <Button size="sm">Get Not-Notion Free</Button>
+            </SignInButton>
+          </>
+        )}
+        {isAuthenticated && !isLoading && (
+          <>
+            <Button variant="ghost" size="sm" asChild>
+              <Link href="/documents">Enter Not-Notion</Link>
+            </Button>
+            <UserButton afterSignOutUrl="/" />
+          </>
+        )}
         <ModeToggle />
       </div>
     </div>
