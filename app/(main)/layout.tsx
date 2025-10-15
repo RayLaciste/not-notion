bf7791540fdab5aec9c88ba763cecc9ca44c8d42
Ray Laciste <raylaciste@yahoo.com>
2025-10-15 15:22:24 -0700

    Add layout to (main) to block unauthenticated access

---
+++ app/(main)/layout.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import { useConvexAuth } from "convex/react";
+import { redirect } from "next/navigation";
+
+import { Spinner } from "@/components/spinner";
+
+const MainLayout = ({ children }: { children: React.ReactNode }) => {
+  const { isAuthenticated, isLoading } = useConvexAuth();
+
+  if (isLoading) {
+    return (
+      <div className="h-full flex items-center justify-center">
+        <Spinner size="large" />
+      </div>
+    );
+  }
+
+  if (!isAuthenticated) {
+    return redirect("/");
+  }
+
+  return <div>{children}</div>;
+};
+
+export default MainLayout;
