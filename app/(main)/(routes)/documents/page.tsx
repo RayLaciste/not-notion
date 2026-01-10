731bf349a5758385abb615918ba698dc8c6d93ac
Ray Laciste <67614706+RayLaciste@users.noreply.github.com>
2026-01-10 00:04:50 -0800

    Add sign out to homepage Add schema for documents Add api to create document

--- app/(main)/(routes)/documents/page.tsx
+++ app/(main)/(routes)/documents/page.tsx
@@ -1,5 +1,39 @@
+"use client";
+
+import Image from "next/image";
+import { useUser } from "@clerk/clerk-react";
+
+import { Button } from "@/components/ui/button";
+import { PlusCircle } from "lucide-react";
+
 const DocumentsPage = () => {
-  return <div>This is a protected page</div>;
+  const { user } = useUser();
+
+  return (
+    <div className="h-full flex flex-col items-center justify-center space-y-4">
+      <Image
+        src="/empty.png"
+        alt="empty"
+        height="300"
+        width="300"
+        className="dark:hidden"
+      />
+      <Image
+        src="/empty-dark.png"
+        alt="empty"
+        height="300"
+        width="300"
+        className="hidden dark:block"
+      />
+      <h2 className="text-large font-medium">
+        Welcome to {user?.firstName}&apos;s Notes
+      </h2>
+      <Button>
+        <PlusCircle className="h-4 w-4 mr-2" />
+        Create a Note
+      </Button>
+    </div>
+  );
 };
 
 export default DocumentsPage;
--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -1,10 +1,12 @@
 "use client";
 
-import { cn } from "@/lib/utils";
 import { ChevronsLeft, MenuIcon } from "lucide-react";
 import { usePathname } from "next/navigation";
 import { ElementRef, useEffect, useRef, useState } from "react";
 import { useMediaQuery } from "usehooks-ts";
+
+import { cn } from "@/lib/utils";
+import { UserItem } from "./user-item";
 
 export const Navigation = () => {
   const pathName = usePathname();
@@ -110,7 +112,7 @@ export const Navigation = () => {
           <ChevronsLeft className="h-6 w-6" />
         </div>
         <div>
-          <p>Action Items</p>
+          <UserItem />
         </div>
         <div className="mt-4">
           <p>Documents</p>
